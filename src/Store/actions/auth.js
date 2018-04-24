c8d1e1e82a0b2dc170df5919e96b0e7d5cdb4ece
Vamsi Devalla <vammichanti40312@gmail.com>
2018-04-24 00:47:50 -0400

    added login functionality to auth form

--- src/Store/actions/auth.js
+++ src/Store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) =>{
     }
 }
 
-export const auth = (email,password)=>{
+export const auth = (email,password,isSignUp)=>{
     return dispatch =>{
         dispatch(authStart())
         const authdata ={
@@ -29,7 +29,10 @@ export const auth = (email,password)=>{
             password: password,
             resturnSecureToken: true
         }
-        axios.post("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDwcLqBfNybGl57SYjRdMVJPW97RuQyshg",authdata)
+        let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDwcLqBfNybGl57SYjRdMVJPW97RuQyshg"
+        if(!isSignUp)
+            url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDwcLqBfNybGl57SYjRdMVJPW97RuQyshg"
+        axios.post(url,authdata)
             .then(res =>{
                 console.log(res)
                 dispatch(authSuccess(res))
@@ -38,5 +41,6 @@ export const auth = (email,password)=>{
                 console.log(err)
                 dispatch(authFail(err))
             })
+
     }
 }
